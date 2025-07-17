32b0b786ca9908378260cf9e230cdca1bea75948
tunamsyar <tun.amsyar@gmail.com>
2025-07-17 22:11:40 +0800

    Vite config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+//
+const allowedHost = 'stellar-sparkle-production.up.railway.app';
 export default defineConfig({
+  server: {
+    host: true, // allow external access
+    allowedHosts: [allowedHost], // <- add this line
+  },
   plugins: [react()],
 })
