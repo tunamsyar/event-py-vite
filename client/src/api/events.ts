4882fab911d8987af25846c8560ee8e1092e82f0
tunamsyar <tun.amsyar@gmail.com>
2025-07-20 00:17:21 +0800

    test 3

--- client/src/api/events.ts
+++ client/src/api/events.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 export const API_URL = (import.meta.env.VITE_API_URL || 'http://localhost:8000').replace(/\/+$/, '')
 
 export type Event = {
